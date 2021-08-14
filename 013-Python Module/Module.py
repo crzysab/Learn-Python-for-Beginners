e1665d1dfcc4f390cefd82e6e78e451749f080e3
sabsabrina <sabrinamanurung92@gmail.com>
2021-08-14 11:58:03 +0700

    Python Module

---
+++ 013-Python Module/Module.py
@@ -0,0 +1,14 @@
+#Example 1
+import samp
+print("Example 1")
+samp.myfunc()
+
+#Example 2
+import samp as sp
+print("Example 2")
+sp.my_func()
+
+#Example 3
+from math import pi,e
+print("Pi : ", pi)
+print("e  : ", e)
---
+++ 013-Python Module/samp.py
@@ -0,0 +1,9 @@
+def myfunc():
+    print("Hello World")
+    print("I'm Sabrina")
+    return ()
+
+def my_func() :
+    print("It's a beautiful day")
+    print("thank you")
+    return ()
