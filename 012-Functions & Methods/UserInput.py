50479b5b1be98db3f9d41f336a80e76539a679a9
sabsabrina <sabrinamanurung92@gmail.com>
2021-08-23 18:33:54 +0700

    Update for Functions & Methods

---
+++ 012-Functions & Methods/UserInput.py
@@ -0,0 +1,11 @@
+#Default DataType for input is 'str'
+a = input("Write the first value  : ")
+b = input("Write the second value : ")
+c = a + b
+print("First result : ",c)
+
+#Set DataType to 'int'
+x = int(input("Write the first value  : "))
+y = int(input("Write the second value : "))
+z = x + y
+print("Second result : ",z)
