e57fd4ad47626fef3bef65ba095ec273cbc9700a
sabsabrina <sabrinamanurung92@gmail.com>
2021-08-26 17:34:48 +0700

    Decision Making Elif Conditions

---
+++ 018-Decision Making/El_If_Conditions.py
@@ -0,0 +1,9 @@
+x = int(input("Input X : "))
+y = int(input("Input Y : "))
+
+if(x == y):
+    print("X and Y is equal")
+elif(x > y):
+    print("X is greater than Y")
+else:
+    print("X is less than Y")
