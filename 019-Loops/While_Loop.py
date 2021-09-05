c00787cf1f5ae089f1cccfb3dc2859cf20a70803
Sabrina Benita <73084687+crzysab@users.noreply.github.com>
2021-09-05 12:03:20 +0700

    Update While_Loop.py

--- 019-Loops/While_Loop.py
+++ 019-Loops/While_Loop.py
@@ -1,5 +1,10 @@
-i = 0 #set nilai awal
-
-while i<6 : #set bahasan looping
-    print("Line -",i)
-    i += 1 #set increment
+# Tagihan
+tagihan = [50000, 75000, 125000, 300000, 200000]
+# Dengan menggunakan while loop
+i = 0 # sebuah variabel untuk mengakses setiap elemen tagihan satu per satu
+jumlah_tagihan = len(tagihan) # panjang (jumlah elemen dalam) list tagihan
+total_tagihan = 0 # mula-mula, set total_tagihan ke 0
+while i < jumlah_tagihan: # selama nilai i kurang dari jumlah_tagihan
+    total_tagihan += tagihan[i]# tambahkan tagihan[i] ke total_tagihan
+    i += 1 # tambahkan nilai i dengan 1 untuk memproses tagihan selanjutnya.
+print(total_tagihan)
